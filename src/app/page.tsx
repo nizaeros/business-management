a2b5cd6ede5a1ed4bcc261c596c98cf33de8e372
Nizam <nizam@duruper.com>
2025-02-19 14:22:56 +0530

    Update root page to follow project structure guidelines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
-export default function Home() {
-  redirect('/login');
-}
+export { default } from './login/page';
+
+export const dynamic = 'force-dynamic';
